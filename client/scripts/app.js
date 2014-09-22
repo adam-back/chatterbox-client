47e2d649d0855b15f7aa3720b25272c50a8d6a61
adam-back <adamback42@gmail.com>
2014-09-22 15:33:02 -0700

    Create cleaning function utilizing jQuery. Tested with first 20 objects from server successfully. S

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,11 @@
 // YOUR CODE HERE:
 var app = {
-  init: function(){},
+  init: function(){
+    //handlers
+    $('.fetch').on('click', function() {
+      app.fetch();
+    });
+  },
   send: function(data){
     $.ajax({
       url: this.server,
@@ -19,15 +24,36 @@ var app = {
       url: this.server,
       type: 'GET',
       success: function(d){
-        // do stuff
-        var clean = JSON.stringify(d);
-        console.log(clean);
+        for(var i = 0; i < 20; i++) {
+          app.clean(d.results[i]);
+        }
       },
       error: function(jqXHR, status, error) {
         console.log(error);
       }
     })
   },
-  server: 'https://api.parse.com/1/classes/chatterbox'
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  clean: function(message) {
+    //cleans one message
+
+    if(message.hasOwnProperty('text') && message.hasOwnProperty('roomname') && message.hasOwnProperty('username')) {
+      var cleaned = {};
+      // start with dirty text, e.g., message.text
+      cleaned.text = $('<span></span>').text(message.text).html();
+      cleaned.roomname = $('<span></span>').text(message.roomname).html();
+      cleaned.username = $('<span></span>').text(message.username).html();
+      cleaned.createdAt = message.createdAt;
+      cleaned.objectId = message.objectId;
+      cleaned.updatedAt = message.updatedAt;
+
+      console.log('dirty', message);
+      console.log('clean', cleaned);
+
+      return cleaned;
+    } else {
+      return null;
+    }
+  }
 
 };
