99eccfef984c064c6904a881e4210cdfe4e7586e
adam-back <adamback42@gmail.com>
2014-09-22 14:37:55 -0700

    App is posting

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,17 @@
 // YOUR CODE HERE:
-
+var app = {
+  init: function(){},
+  send: function(data){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      success: function(d){ consol.el},
+      error: function(jqXHR, status, error) {
+        console.log(error);
+      }
+    })
+  },
+  fetch: function(){}
+};
