1e77467e5a1d0a2bbc847d18aea1499cb112251e
adam-back <adamback42@gmail.com>
2014-09-22 16:34:05 -0700

    Gets appearning on screen in SVG element.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,7 +9,8 @@ var app = {
 
     app.svg = d3.select('#canvas').append('svg')
       .attr('height', $(window).height() * 0.75)
-      .attr('width', 500);
+      .attr('width', 500)
+      .attr('id', 'chats');
   },
 
   send: function(data){
@@ -30,11 +31,7 @@ var app = {
     $.ajax({
       url: this.server,
       type: 'GET',
-      success: function(d){
-        for(var i = 0; i < 20; i++) {
-          app.clean(d.results[i]);
-        }
-      },
+      success: app.update,
       error: function(jqXHR, status, error) {
         console.log(error);
       }
@@ -58,7 +55,27 @@ var app = {
 
       return cleaned;
     } else {
+
       return null;
     }
+  },
+
+  update: function(successData) {
+    var data = successData.results;
+
+    for(var i = 0; i < data.length; i++) {
+      var clean = app.clean(data[i]);
+      if(clean) {
+        data[i] = clean;
+      } else {
+        data.splice(i, 1);
+      }
+    }
+
+    var messages = app.svg.selectAll('.message').data(data);
+    messages.enter()
+      .append('g').attr('class', 'message')
+      .attr('transform', function(d,i){return 'translate(0,' + (20 * (i + 1)) + ')';})
+      .append('text').text(function(d){return JSON.stringify(d);});
   }
 };
