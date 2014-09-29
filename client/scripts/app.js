512e1c7da2bcdcc4eb23d6f2e060ffc0e86f20a0
Adam Back <adb33@email.arizona.edu>
2014-09-28 21:56:38 -0700

    Successfully created GET request and added server as key to app.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,8 @@
 // YOUR CODE HERE:
 var app = {
+
+	server: 'https://api.parse.com/1/classes/chatterbox',
+	
 	init: function() {
 
 	},
@@ -9,9 +12,23 @@ var app = {
 			url: 'https://api.parse.com/1/classes/chatterbox',
 			data: JSON.stringify(data),
 			type: 'POST',
+			contentType: 'application/json',
 			success: function() {
 				console.log('Posted successfully');
 			}
 		});
-	}
+	},
+
+	fetch: function(data) {
+		$.ajax({
+			url: 'https://api.parse.com/1/classes/chatterbox',
+			type: 'GET',
+			contentType: 'application/json',
+			data: data,
+			success: function(data) {
+				console.log('Fetched successfully:' + data);
+			}
+		});
+	},
+
 }
