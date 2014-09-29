7aa404f79166f0422d6ebca3bbdebdbee90bbdf7
Adam Back <adb33@email.arizona.edu>
2014-09-28 21:49:10 -0700

    Installed necessary dependencies using bower. Also added an app wrapper with methods init and send. Send makes a POST request via ajax.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,17 @@
 // YOUR CODE HERE:
+var app = {
+	init: function() {
 
+	},
+
+	send: function(data) {
+		$.ajax({
+			url: 'https://api.parse.com/1/classes/chatterbox',
+			data: JSON.stringify(data),
+			type: 'POST',
+			success: function() {
+				console.log('Posted successfully');
+			}
+		});
+	}
+}
